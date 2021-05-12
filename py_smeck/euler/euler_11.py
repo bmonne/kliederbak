d0ce0e437350128c22ea94975e7a569fd25e367d
monneb <monneb@noord-holland.nl>
2021-05-12 19:19:09 +0200

    Verderrr

--- py_smeck/euler/euler_11.py
+++ py_smeck/euler/euler_11.py
@@ -2,8 +2,18 @@ import pandas as pd
 
 data = pd.read_csv('euler_11_grid.csv', header=None)
 
+# calculate horizontal products
+for i in range(1,20):
+    df = data[i:i+4]
+    prod = (df.Series([]).prod())
+
+
+# calculate vertical products
 for i in range(1,20):
     df = data[i:i+4]
     print(df)
 
-
+# calculate diagogal products
+for i in range(1,20):
+    df = data[i:i+4]
+    print(df)
